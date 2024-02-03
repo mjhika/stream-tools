80c2cdac93f148e69f5e6b0d88273dcd9d1af4e7
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-02-03 00:42:14 -0500

    add animations component

---
+++ components/animations/src/mjhika/stream/animations/interface.clj
@@ -0,0 +1,1 @@
+(ns mjhika.stream.animations.interface)
---
+++ components/animations/test/mjhika/stream/animations/interface_test.clj
@@ -0,0 +1,6 @@
+(ns mjhika.stream.animations.interface-test
+  (:require [clojure.test :as test :refer :all]
+            [mjhika.stream.animations.interface :as animations]))
+
+(deftest dummy-test
+  (is (= 1 1)))
